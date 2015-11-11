d2bfb4437d1446b03969399879165351debb099b
Mihai <mihai@physik.uzh.ch>
2015-11-11 17:08:24 +0100

    energy plots

--- py_wrap/m_global.py
+++ py_wrap/m_global.py
@@ -1,10 +1,25 @@
 from matplotlib import pyplot
 import numpy
+import m_analyse
 
-file_name = ""
-def read_energy(file_name):
-	data = numpy.genfromtxt(file_name, delimiter = ' ', skip_footer = 0)
-	print data[0]
+def plot_energy(d):
+	path = d['output_path']
+	efile= d['output_e']
+	file_name = m_analyse.reove_spaces(path+efile)
+	out_name  = m_analyse.remove_spaces(path+"total_energy_cons.pdf")
+
+	if m_analyse.remove_spaces(d['energy']) == 'yes': 
+		data = numpy.genfromtxt(file_name, delimiter = ' ', skip_footer = 0)
+		tdata = numpy.transpose(data)
+		time =  tdata[0]
+		energy = tdata[1]
+		pyplot.figure(figsize = (10,10))
+		pyplot.plot(time, energy/10**6, 'r.')
+		pyplot.xlabel("time [c.u.]", fontsize = 20)
+		pyplot.ylabel("Energy in the system [MJ]", fontsize = 20)
+		pyplot.tick_params(axis='both', which='major', labelsize=15)
+		pyplot.tick_params(axis='both', which='minor', labelsize=12)
+		pyplot.savefig(out_name)
+	
 	
 
-read_energy(file_name)
--- py_wrap/wrap.py
+++ py_wrap/wrap.py
@@ -4,7 +4,7 @@ from sys import argv
 import re
 from m_execute import read_parameter, execute_it
 import m_analyse
-
+import m_global
 
 	
 script, input = argv
@@ -13,6 +13,8 @@ d = read_parameter(input)
 if m_analyse.remove_spaces(d['execute']) == 'yes':
 	execute_it(d)
 
-m_analyse.plott_all(d)
 
+
+m_analyse.plott_all(d)
+m_global.plot_energy(d)
 
